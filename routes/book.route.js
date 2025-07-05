c91241ad07e752b61c0951af9a9f3ce69b9e62c6
krish kumar <krishchahit1605@gmail.com>
2025-07-05 12:37:46 +0530

    buy router

--- routes/book.route.js
+++ routes/book.route.js
@@ -1,10 +1,11 @@
 import express from "express";
 import { Router } from "express";
-import { addBooks, getBook } from "../controller/book.controller.js";
+import { addBooks, getBook, viewBook } from "../controller/book.controller.js";
 
 const router = Router();
 
 router.get("/",getBook);
 router.post("/add-books",addBooks)
+router.get("/:id",viewBook);
 
 export default router
