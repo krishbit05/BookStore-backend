aa0cb328c478300e41150dca34936cd687102ebe
krish kumar <krishchahit1605@gmail.com>
2025-07-05 09:55:37 +0530

    add-books

--- controller/book.controller.js
+++ controller/book.controller.js
@@ -1,11 +1,38 @@
 import Book from "../model/book.model.js"
 
-export const getBook = async(req,res)=>{
-    try{
+export const getBook = async (req, res) => {
+    try {
         const book = await Book.find();
         res.status(202).json(book);
-    }catch{
-        console.log("Error:",error);
+    } catch {
+        console.log("Error:", error);
         res.status(500).json(error);
+    }
+}
+
+export const addBooks = async (req, res) => {
+    try {
+        const { name, title, price, category, image } = req.body;
+
+        if (!name || !title || !price || !category || !image) {
+            return res.status(400).json({ message: "All fields are required" });
+        }
+
+        const createBook = new Book({
+            name,
+            title,
+            price,
+            category,
+            image,
+        })
+        const savedBook = await createBook.save();
+        res.status(201).json({
+            message: "Book insert Successfully",
+            book: savedBook
+        })
+
+    } catch (error) {
+        console.log("Error: " + error.message);
+        res.status(500).json({ message: "Internal server error" });
     }
 }
--- routes/book.route.js
+++ routes/book.route.js
@@ -1,9 +1,10 @@
 import express from "express";
 import { Router } from "express";
-import { getBook } from "../controller/book.controller.js";
+import { addBooks, getBook } from "../controller/book.controller.js";
 
 const router = Router();
 
 router.get("/",getBook);
+router.post("/add-books",addBooks)
 
 export default router
