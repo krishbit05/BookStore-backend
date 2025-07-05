a304fbe8afeb35ba25b80c83744a9fe8f8186079
krish kumar <krishchahit1605@gmail.com>
2025-07-05 13:51:17 +0530

    add cart remove buy

--- controller/book.controller.js
+++ controller/book.controller.js
@@ -36,13 +36,3 @@ export const addBooks = async (req, res) => {
         res.status(500).json({ message: "Internal server error" });
     }
 }
-
-export const viewBook = async(req,res)=>{
-    try {
-        const book = await Book.findById(req.params.id);
-        if (!book) return res.status(404).json({ message: "Book not found" });
-        res.status(200).json(book);
-      } catch (err) {
-        res.status(500).json({ message: "Error retrieving book", error: err });
-      }
-}
--- routes/book.route.js
+++ routes/book.route.js
@@ -1,11 +1,10 @@
 import express from "express";
 import { Router } from "express";
-import { addBooks, getBook, viewBook } from "../controller/book.controller.js";
+import { addBooks, getBook } from "../controller/book.controller.js";
 
 const router = Router();
 
 router.get("/",getBook);
 router.post("/add-books",addBooks)
-router.get("/:id",viewBook);
 
 export default router
